99495d909e0979415bcbe8649b0fb9182f129682
Suley <suleyliu@gmail.com>
2020-11-14 14:44:23 -0600

    Try to add Name field in resigster

--- BugTracker/Models/AccountViewModels.cs
+++ BugTracker/Models/AccountViewModels.cs
@@ -64,6 +64,10 @@ namespace BugTracker.Models
 
     public class RegisterViewModel
     {
+        //[Required]
+        //[Display(Name = "Name")]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        //public string Name { get; set; }
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]

6b2bae57da0fab200e4de9367b4fdab3589fb7b1
Milijana Tomic <mlgalic@gmail.com>
2020-11-15 01:59:18 -0600

    CreatedTicketController

---
+++ BugTracker/DAL/TicketHelper.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.DAL
+{
+    public static class TicketHelper
+    {
+        public static List<Ticket> GetTickets()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var tickets = db.Tickets.Include("Project").Include("TicketPriority").Include("TicketStatus").Include("TicketType");
+            return tickets.ToList();
+        }
+    }
+}

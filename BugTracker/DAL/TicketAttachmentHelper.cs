6d7ba7407cefb806f558bb1912fe4a5437a71982
Willy Gilbert <willyluciano@gmail.com>
2020-11-16 20:56:45 -0600

    Added FileUploader

--- BugTracker/DAL/TicketAttachmentHelper.cs
+++ BugTracker/DAL/TicketAttachmentHelper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -9,65 +10,61 @@ namespace BugTracker.DAL
 {
     public class TicketAttachmentHelper
     {
-        public static List<TicketAttachment> GetTicketAttachments()
+        public static List<TicketAttachment> GetTicketAttachments(int ticketId)
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            var TicketAttachment = db.TicketAttachments.Include(t => t.Ticket).Include(t => t.User);
+            var TicketAttachment = db.TicketAttachments.Include(t => t.Ticket).Include(t => t.User).Where(t => t.TicketId == ticketId);
             return TicketAttachment.ToList();
+        }
+
+        public static Message Create(int ticketId, HttpPostedFileBase filePath, string description, string userId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            Message message = FileUploaderHelper.Uploader(ticketId, filePath);
+            string fileUrl = message.FileName;
+            string fileName = Path.GetFileName(fileUrl);
+
+            if (message.Code == MessageType.Successful)
+            {
+                TicketAttachment ticketAttachment = new TicketAttachment
+                {
+                    TicketId = ticketId,
+                    FilePath = fileName,
+                    Description = description,
+                    Created = DateTime.Now,
+                    UserId = userId,
+                    FileUrl = fileUrl
+                };
+                db.TicketAttachments.Add(ticketAttachment);
+                db.SaveChanges();
+                db.Dispose();
+                return message;
+            }
+            else
+            {
+                return message;
+            }
         }
 
         public static TicketAttachment GetTicketAttachment(int? Id)
         {
             ApplicationDbContext db = new ApplicationDbContext();
             TicketAttachment TicketAttachment = db.TicketAttachments.Find(Id);
-            db.Dispose();
             if (TicketAttachment == null)
             {
                 return null;
             }
-            db.Dispose();
             return TicketAttachment;
-        }
-
-        public static void Create(int ticketId, string filePath, string description, DateTime created, string userId, string fileUrl)
-        {
-            ApplicationDbContext db = new ApplicationDbContext();
-            TicketAttachment ticketAttachment = new TicketAttachment
-            {
-                TicketId = ticketId,
-                FilePath = filePath,
-                Description = description,
-                Created = created,
-                UserId = userId,
-                FileUrl = fileUrl
-            };
-            db.TicketAttachments.Add(ticketAttachment);
-            db.SaveChanges();
-            db.Dispose();
-        }
-
-        public static void Edit(int id, int ticketId, string filePath, string description, DateTime created, string userId, string fileUrl)
-        {
-            ApplicationDbContext db = new ApplicationDbContext();
-            TicketAttachment ticketAttachment = GetTicketAttachment(id);
-            ticketAttachment.TicketId = ticketId;
-            ticketAttachment.FilePath = filePath;
-            ticketAttachment.Description = description;
-            ticketAttachment.Created = created;
-            ticketAttachment.UserId = userId;
-            ticketAttachment.FileUrl = fileUrl;
-            db.Entry(ticketAttachment).State = EntityState.Modified;
-            db.SaveChanges();
-            db.Dispose();
         }
 
         public static void Delete(int id)
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            TicketAttachment ticketAttachment = GetTicketAttachment(id);
+            TicketAttachment ticketAttachment = db.TicketAttachments.Find(id);
             db.TicketAttachments.Remove(ticketAttachment);
             db.SaveChanges();
             db.Dispose();
         }
+
     }
 }
